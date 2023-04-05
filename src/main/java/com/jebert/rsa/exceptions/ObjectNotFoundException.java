cc23c39893cd72dbef0ed858788f9cd33c4008f0
jebert <jonatan.ebert@gmail.com>
2023-04-04 23:44:46 -0300

    Create Class UserService that implements UserDetailsService

---
+++ src/main/java/com/jebert/rsa/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,15 @@
+package com.jebert.rsa.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException (String msg) {
+        super(msg);
+    }
+
+    public ObjectNotFoundException (String msg, Throwable Cause) {
+
+    }
+
+}
---
+++ src/main/java/com/jebert/rsa/user/service/UserService.java
@@ -0,0 +1,60 @@
+package com.jebert.rsa.user.service;
+
+
+import com.jebert.rsa.exceptions.ObjectNotFoundException;
+import com.jebert.rsa.user.model.User;
+import com.jebert.rsa.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public UserService() {}
+
+    public List<User> findAllUsers() {
+        return repository.findAll();
+    }
+
+    public Optional<User> findUserById (UUID id){
+        Optional<User> User = repository.findById(id);
+        return Optional.of(User.orElseThrow(()-> new ObjectNotFoundException("User not found with id:" + id.toString())));
+    }
+
+    public User findUserByUsername (String username){
+        var user = repository.findByUserName(username);
+        return user;
+    }
+
+    @Transactional
+    public User saveUser (User User) {
+        return repository.save(User);
+    }
+
+    public void delete(UUID uuid) {
+        repository.delete(findUserById(uuid).get());
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        User user = findUserByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found"+ username);
+        }
+
+        return user;
+    }
+}
