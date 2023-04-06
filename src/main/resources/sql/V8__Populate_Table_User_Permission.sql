e0271515aefa47e39cb1df2783eae08ad7c4a29d
jebert <jonatan.ebert@gmail.com>
2023-04-06 00:16:01 -0300

    Populated tables users and user_permission

---
+++ src/main/resources/sql/V8__Populate_Table_User_Permission.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.user_permission (id_user, id_permission) VALUES ('e453953e-7e5d-4b9e-9522-452b94aa2e03', '5adf4573-6c18-4c60-a9e1-1f36952175e9');
+INSERT INTO public.user_permission (id_user, id_permission) VALUES ('4164ecce-8852-4701-8fc2-3fca23b985ae', '006c0585-12f9-4eb1-b50e-18518e1ffc76');
+INSERT INTO public.user_permission (id_user, id_permission) VALUES ('b195a9be-9ed8-47a8-acc6-66a772b55aed', '16126a0a-4a00-4e7c-add1-b2201ca14294');
+INSERT INTO public.user_permission (id_user, id_permission) VALUES ('924e884a-9706-4522-a153-cba137e2c087', 'd50deb23-9aad-46da-89f2-c6ec30fad77d');
